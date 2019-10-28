2af25eabdd48eaca253ba809bcb9b258fe59279f
darong123 <sin40dh73@gmail.com>
2019-10-28 10:59:05 +0900

    midterm

--- 201921005midterm-3.c
+++ 201921005midterm-3.c
@@ -6,9 +6,9 @@ int main(void){
   printf("a=%d,b=%d",a,b);
   int *num1 = &a;
   int *num2 = &b;
-  int *temp = num1;
-  num1 = num2;
-  num2 = temp;
+  int *temp = *num1;
+  *num1 = *num2;
+  *num2 = temp;
   printf("a=%d,b=%d",a,b);
   return 0;
 }
